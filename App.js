17718f381c0b1a6cb2bb1e9a8b006a1673e98841
Wilson Santos Carvalho <wilson@MacBook-Pro-de-Wilson.local>
2021-03-02 15:03:19 -0300

    chore: success and add point screens created

--- App.js
+++ App.js
@@ -2,8 +2,12 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
+import Routes from './src/routes';
+
 export default function App() {
   return (
-    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
   );
 }
---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const Home = () => (
+  <View>
+    <Text>Teste</Text>
+  </View>
+);
+
+export default Home;
